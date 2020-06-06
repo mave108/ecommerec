8d54d982fbbbb88ebadd5145b91ff00f16190e00
Rakesh <rakeshkumar8900@gmail.com>
2020-06-06 12:05:49 +0530

    prettier config + styled component babel tweek

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    quoteProps: 'as-needed',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false,
+    endOfLine: 'auto',
+};
