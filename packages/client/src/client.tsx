86e2f784ed4e095df92dafb8a23186bc626574ca
Rakesh <rakeshkumar8900@gmail.com>
2020-06-06 15:47:40 +0530

    grommet design system incorporation

--- packages/client/src/client.tsx
+++ packages/client/src/client.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import { hydrate } from "react-dom";
-import Loadable from "react-loadable";
-import { BrowserRouter } from "react-router-dom";
-import { App } from "./components/App";
-import "./style.css";
+import React from 'react';
+import { hydrate } from 'react-dom';
+import Loadable from 'react-loadable';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './components/App';
+import './style.css';
+import { Grommet } from 'grommet';
+import { aruba } from './theme/aruba';
 
 const component = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <Grommet theme={aruba}>
+      <App />
+    </Grommet>
+  </BrowserRouter>
 );
 
-Loadable.preloadReady()
-.then(() => {
-    hydrate(component, document.getElementById("root"));
+Loadable.preloadReady().then(() => {
+  hydrate(component, document.getElementById('root'));
 });
--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { Attraction, Car, TreeOption } from 'grommet-icons';
+import { Grommet, Box, Carousel } from 'grommet';
 
 export class HomePage extends React.Component<any, any> {
   public render() {
-    return <div>Home Page</div>;
+    return (
+      <div>
+        <CustomCarousel />
+        Home Page
+      </div>
+    );
   }
 }
 
 export default hot(module)(HomePage);
+
+const customTheme = {
+  carousel: {
+    animation: {
+      duration: 400,
+    },
+    icons: {
+      color: 'blue',
+    },
+    disabled: {
+      icons: {
+        color: 'grey',
+      },
+    },
+  },
+};
+
+const CustomCarousel = ({ ...rest }) => (
+  <Grommet theme={customTheme}>
+    <Box align="center" pad="large">
+      <Carousel {...rest}>
+        <Box pad="xlarge" background="accent-1">
+          <Attraction size="xlarge" />
+        </Box>
+        <Box pad="xlarge" background="accent-2">
+          <TreeOption size="xlarge" />
+        </Box>
+        <Box pad="xlarge" background="accent-3">
+          <Car size="xlarge" />
+        </Box>
+      </Carousel>
+    </Box>
+  </Grommet>
+);
