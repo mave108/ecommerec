98f1a6de38dccc6bbc5b38a9eec644f26d71d2ae
Rakesh <rakeshkumar8900@gmail.com>
2020-06-06 15:24:22 +0530

    few updattion

--- packages/client/src/pages/AboutPage.tsx
+++ packages/client/src/pages/AboutPage.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { hot } from "react-hot-loader";
-import "./About.css";
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './About.css';
 
 export class AboutPage extends React.Component<any, any> {
   public render() {
-    return (
-      <div>
-        About Page
-      </div>
-    );
+    return <div>About Page</div>;
   }
 }
 
--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { hot } from "react-hot-loader";
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
 export class HomePage extends React.Component<any, any> {
   public render() {
-    return (
-      <div>
-        Home Page
-      </div>
-    );
+    return <div>Home Page</div>;
   }
 }
 
